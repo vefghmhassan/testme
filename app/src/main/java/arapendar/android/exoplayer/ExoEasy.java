b8624e155c64f6fced149c4dfe051143e617d94e
vefghmhassan <vefghmhassan@gmail.com>
2019-02-25 14:16:22 +0330

    new  code

---
+++ app/src/main/java/arapendar/android/exoplayer/ExoEasy.java
@@ -0,0 +1,85 @@
+package arapendar.android.exoplayer;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultAllocator;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class ExoEasy {
+    public static Context contextme;
+
+
+
+     static    SimpleExoPlayer player;
+    public static final int MIN_BUFFER_DURATION = 3000;
+    public static final int MAX_BUFFER_DURATION = 5000;
+    public static final int MIN_PLAYBACK_START_BUFFER = 1500;
+    public static final int MIN_PLAYBACK_RESUME_BUFFER = 5000;
+    static PlayerView playerViewme;
+
+    public static void intializ(PlayerView playerView, String url, Context context) {
+        playerViewme = playerView;
+        contextme = context;
+        initializePlayer();
+        if (url == null) {
+
+        } else {
+            buildMediaSource(Uri.parse(url));
+        }
+
+    }
+
+    private static void initializePlayer() {
+        if (player == null) {
+            // 1. Create a default TrackSelector
+            LoadControl loadControl = new DefaultLoadControl(
+                    new DefaultAllocator(true, 16),
+                    MIN_BUFFER_DURATION,
+                    MAX_BUFFER_DURATION,
+                    MIN_PLAYBACK_START_BUFFER,
+                    MIN_PLAYBACK_RESUME_BUFFER, -1, true);
+
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            TrackSelection.Factory videoTrackSelectionFactory =
+                    new AdaptiveTrackSelection.Factory(bandwidthMeter);
+            TrackSelector trackSelector =
+                    new DefaultTrackSelector(videoTrackSelectionFactory);
+            // 2. Create the player
+            player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(contextme), trackSelector, loadControl);
+            playerViewme.setPlayer(player);
+        }
+    }
+
+    private  static void buildMediaSource(Uri mUri) {
+        // Measures bandwidth during playback. Can be null if not required.
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        // Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(contextme,
+                Util.getUserAgent(contextme, ""), bandwidthMeter);
+        // This is the MediaSource representing the media to be played.
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(mUri);
+        // Prepare the player with the source.
+        player.prepare(videoSource);
+        player.setPlayWhenReady(true);
+
+
+    }
+}
